47d683b072e0981a34a34f016aaca7e531d06de2
zachmichael14 <zachmichael14@gmail.com>
2022-02-12 18:21:13 -0600

    Initial commit

---
+++ auctions/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'auctions'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
---
+++ users/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models.deletion import CASCADE, PROTECT
+
+# Create your models here.
