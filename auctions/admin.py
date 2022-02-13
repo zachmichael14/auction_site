2fbbf91d164bca50dba2ea19a2fce6cd832be261
zachmichael14 <zachmichael14@gmail.com>
2022-02-12 18:42:13 -0600

    Register auctions.models with admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import Category, Listing, Bid, Watchlist
 # Register your models here.
+
+# Allow editing of bids from listing admin page
+class BidTabularInline(admin.TabularInline):
+    model = Bid
+
+
+class ListingAdmin(admin.ModelAdmin):
+    model = Listing
+    list_display = ["title", "creation_timestamp", "category"]
+    list_filter = ["creation_timestamp", "category"]
+    inlines = [BidTabularInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ["id", "category"]
+
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Bid)
+admin.site.register(Watchlist)
