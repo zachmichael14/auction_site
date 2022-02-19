10a596b77db6062c7974b7803e5f4907d54f3a72
zachmichael14 <zachmichael14@gmail.com>
2022-02-19 14:58:32 -0600

    Create validators.py

---
+++ auctions/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import Bid
+
+
+class BidForm(forms.ModelForm):
+    class Meta:
+        model = Bid
+        exclude = ["listing", "bidder", "timestamp"]
+
+    amount = forms.CharField(label="",
+        widget = forms.NumberInput(
+            attrs = {
+                "class": "border-0 py-2 w-100 price",
+                "name": "bid_form-amount",
+                "placeholder": "Bid ($0.01-$500.00)"
+            }
+        )
+    )
