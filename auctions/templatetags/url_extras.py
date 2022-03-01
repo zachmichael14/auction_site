3e26b98beb965ceb9b8abd3811d603333c35b17a
zachmichael14 <zachmichael14@gmail.com>
2022-02-28 21:41:46 -0600

    Write query_params template tag

---
+++ auctions/templatetags/url_extras.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def query_params(context, **kwargs):
+    # Get current URL query parameters
+    d = context['request'].GET.copy()
+    # Add key:value pairs from tag function call
+    for k, v in kwargs.items():
+        d[k] = v
+    # Return encoded dictionary containing updated query params
+    return d.urlencode()
