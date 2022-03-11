20137d31b06eec2e591d74ee3fb6ae143f623015
zachmichael14 <zachmichael14@gmail.com>
2022-03-10 21:00:30 -0600

    Remove Profile model

--- users/models.py
+++ users/models.py
@@ -15,24 +15,3 @@ class AuctionUser(AbstractUser):
             if img.height > 300 or img.width > 300:
                 img.thumbnail(size)
                 img.save(self.image.path)
-
-
-
-
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     image = models.ImageField(default='default_profile_pic.jpeg', upload_to='profile_pics')
-
-#     def __str__(self):
-#         return f"{self.user.username}'s profile"
-
-#     def save(self, *args, **kwargs):
-#         super().save(*args, **kwargs)
-
-#         # Create profile picture thumbnail of size, then save
-#         size = (300, 300)
-#         with Image.open(self.image.path) as img:
-#             if img.height > 300 or img.width > 300:
-#                 img.thumbnail(size)
-#                 img.save(self.image.path)
