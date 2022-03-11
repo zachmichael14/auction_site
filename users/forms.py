b0cf679ec678236cacd401c2c07afb7bdbe3f89f
zachmichael14 <zachmichael14@gmail.com>
2022-03-10 20:43:09 -0600

    Remove Profile import statement

--- users/forms.py
+++ users/forms.py
@@ -2,8 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from users.models import Profile
-
 
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=150)
