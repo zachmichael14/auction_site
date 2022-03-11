2f7c1297b6f5dcdfbcf1d83897e57c721c21dd18
zachmichael14 <zachmichael14@gmail.com>
2022-03-10 19:43:11 -0600

    Remove unused is_seller Listing method

--- auctions/models.py
+++ auctions/models.py
@@ -1,12 +1,10 @@
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
-from django.db.models.deletion import CASCADE, PROTECT
 
-from .managers import CategoryManager, ListingManager
-
-from .validators import validate_bid
+from auctions.managers import CategoryManager
+from auctions.managers import ListingManager
 
 
 class Category(models.Model):
@@ -18,22 +16,22 @@ class Category(models.Model):
 
 
 class Listing(models.Model):
-    seller = models.ForeignKey(User, on_delete=CASCADE, related_name='user_listings')
-    category = models.ForeignKey(Category, on_delete=PROTECT, related_name='listings')
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_listings')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='listings')
     title = models.CharField(max_length=64)
     description = models.TextField()
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(blank=True, default='no_image.png', upload_to='listing_pics')
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
     creation_timestamp = models.DateTimeField(auto_now_add=True)
     duration = models.PositiveIntegerField(
         validators=[
             MinValueValidator(3, message='Duration must be between 3 and 31 days (inclusive).'),
-            MaxValueValidator(31, message='Duration must be between 3 and 31 days (inclusive).')
+            MaxValueValidator(31, message='Duration must be between 3 and 31 days (inclusive).'),
         ]
     )
     end_date = models.DateField()
     is_active = models.BooleanField(default=True)
-    winner = models.ForeignKey(User, blank=True, null=True, on_delete=PROTECT, related_name='won')
+    winner = models.ForeignKey(User, blank=True, null=True, on_delete=models.PROTECT, related_name='won')
     objects = ListingManager()
 
     def __str__(self):
@@ -51,11 +49,6 @@ class Listing(models.Model):
             return Bid.objects.filter(listing=self.id).order_by('-amount').first()
         return self.starting_bid
 
-    def is_seller(self, user=None):
-        if user == self.seller:
-            return True
-        return False
-
     def is_watched(self, user):
         if Watchlist.objects.filter(user=user, listing=self).exists():
             return True
@@ -63,8 +56,8 @@ class Listing(models.Model):
 
     
 class Bid(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=CASCADE, related_name='bids')
-    bidder = models.ForeignKey(User, on_delete=CASCADE, related_name='placed_bids')
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name='placed_bids')
     timestamp = models.DateTimeField(auto_now_add=True)
     amount = models.DecimalField(
         max_digits=19, 
@@ -76,8 +69,8 @@ class Bid(models.Model):
 
 
 class Watchlist(models.Model):
-    user = models.ForeignKey(User, on_delete=CASCADE, related_name='my_watchlist')
-    listing = models.ForeignKey(Listing, on_delete=CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='my_watchlist')
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user} watching post {self.listing}'
