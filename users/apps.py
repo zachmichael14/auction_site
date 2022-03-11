57a4fa615660f61a824f933cf0e7d4c61874822c
zachmichael14 <zachmichael14@gmail.com>
2022-03-10 20:41:26 -0600

    Remove call to signals.py

--- users/apps.py
+++ users/apps.py
@@ -5,6 +5,3 @@ class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
-    def ready(self):
-        # Implicitly connect signal handlers decorated with @receiver.
-        from . import signals
