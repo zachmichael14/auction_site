f502d819667c486fac5d7f4fada7a0b3233a1ae1
zachmichael14 <zachmichael14@gmail.com>
2022-03-10 20:44:13 -0600

    Remove reference to ProfileUpdateForm

--- users/views.py
+++ users/views.py
@@ -28,12 +28,10 @@ def register(request):
 def profile(request):
 
     u_update_form = UserUpdateForm()
-    p_update_form = ProfileUpdateForm()  
 
     context = {
         'active_listings': Listing.objects.exclude(is_active=False).filter(seller=request.user),
         'u_update_form': u_update_form,
-        'p_update_form': p_update_form
     }
 
     return render(request, 'users/profile.html', context)
