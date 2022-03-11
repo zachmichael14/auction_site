b1b48b7a7a0f6f0f94518641be5f23e18e599042
zachmichael14 <zachmichael14@gmail.com>
2022-03-10 19:44:38 -0600

    Create ListingUpdateForm

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,8 +1,8 @@
-from faulthandler import disable
 from django import forms
-from decimal import Decimal
 
-from .models import Bid, Listing, Category
+from .models import Bid
+from .models import Category
+from .models import Listing
 
 
 class BidForm(forms.ModelForm):
@@ -16,34 +16,30 @@ class BidForm(forms.ModelForm):
             attrs = {
                 'class': 'border-0 py-2 w-100 price',
                 'name': 'bid_form-amount',
-                'placeholder': 'Bid'
+                'placeholder': 'Bid',
             }
         )
     ) 
 
-    def clean(self):
-        cleaned_data = super().clean()
-        print(self.cleaned_data)
-        listing = cleaned_data.get('listing')
-        amount = Decimal(cleaned_data.get('amount'))
-        if amount < listing.top_bid.amount:
-            raise forms.ValidationError({'amount': 'Your bid must be greater than the current top bid.'})
-        elif amount > listing.top_bid.amount + 500:
-            raise forms.ValidationError({'amount': 'Your bid cannot be more than $500 higher the current top bid.'})
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     print(self.cleaned_data)
+    #     listing = cleaned_data.get('listing')
+    #     amount = Decimal(cleaned_data.get('amount'))
+    #     if amount < listing.top_bid.amount:
+    #         raise forms.ValidationError({'amount': 'Your bid must be greater than the current top bid.'})
+    #     elif amount > listing.top_bid.amount + 500:
+    #         raise forms.ValidationError({'amount': 'Your bid cannot be more than $500 higher the current top bid.'})
 
 
-class ListingForm(forms.ModelForm):
-    class Meta:
-        model = Listing     
-        fields = ['title', 'description', 'category', 'image', 'duration', 'starting_bid']
-
+class ListingCreateForm(forms.ModelForm):
     title = forms.CharField(
         max_length=64,
         widget = forms.TextInput(
             attrs = {
                 'class': 'border w-100 p-2 bg-white',
                 'name': 'listing_form-title',
-                'placeholder': 'Title'
+                'placeholder': 'Title',
             }
         )
     )
@@ -65,7 +61,7 @@ class ListingForm(forms.ModelForm):
                 'id': 'inputGroupSelect',
                 'class': 'w-100',
                 'name': 'listing_form-category',
-                'placeholder': 'Category'
+                'placeholder': 'Category',
             }
         )
     )
@@ -81,11 +77,69 @@ class ListingForm(forms.ModelForm):
         widget=forms.NumberInput(
             attrs={
                 'class': 'border-0 py-2 w-100 price',
-                'placeholder': 'Starting bid'
+                'placeholder': 'Starting bid',
             }
         )
     )
- 
+
+    class Meta:
+        model = Listing     
+        fields = ['title', 'description', 'category', 'image', 'duration', 'starting_bid']
+
+
+class ListingUpdateForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=64,
+        widget = forms.TextInput(
+            attrs = {
+                'class': 'border w-100 p-2 bg-white',
+                'name': 'listing_form-title',
+                'placeholder': 'Title',
+            }
+        )
+    )
+    description = forms.CharField(
+        max_length=500,
+        widget = forms.Textarea(
+            attrs = {
+                'class': 'border p-3 w-100',
+                'name': 'listing_form-description',
+                'placeholder': 'Write details about your product',
+            }
+        )
+    )
+    category= forms.ModelChoiceField(
+        empty_label='Select a category',
+        queryset=Category.objects.all().order_by('name'),
+        widget = forms.Select(
+            attrs = {
+                'id': 'inputGroupSelect',
+                'class': 'w-100',
+                'name': 'listing_form-category',
+                'placeholder': 'Category',
+            }
+        )
+    )
+    duration = forms.IntegerField(
+        widget=forms.NumberInput(
+            attrs={
+                'class': 'border-0 py-2 w-100 price',
+                'placeholder': 'Duration (3-31 days)',
+            }
+        )
+    )
+    image = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'enctype': 'multipart/form-data',
+            }
+        )
+    )
+
+    class Meta:
+        model = Listing     
+        fields = ['title', 'description', 'category', 'image', 'duration']
+    
 class SearchForm(forms.Form):
     q_string = forms.CharField(
         label='',
@@ -94,7 +148,7 @@ class SearchForm(forms.Form):
             attrs={
                 'id': 'inputtext4',
                 'class': 'form-control my-2 my-lg-1',
-                'placeholder': 'What can we help you find?'
+                'placeholder': 'What can we help you find?',
             }
         )
     )
