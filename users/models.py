a914a62432b0337d7db9f9ad93f1f0dc8835f2f8
zachmichael14 <zachmichael14@gmail.com>
2022-03-10 20:21:59 -0600

    Create AuctionUser model
    Profile model is now defunct, comment out for now (then delete).

--- users/models.py
+++ users/models.py
@@ -1,14 +1,10 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from PIL import Image
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class AuctionUser(AbstractUser):
     image = models.ImageField(default='default_profile_pic.jpeg', upload_to='profile_pics')
-
-    def __str__(self):
-        return f"{self.user.username}'s profile"
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -19,3 +15,24 @@ class Profile(models.Model):
             if img.height > 300 or img.width > 300:
                 img.thumbnail(size)
                 img.save(self.image.path)
+
+
+
+
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     image = models.ImageField(default='default_profile_pic.jpeg', upload_to='profile_pics')
+
+#     def __str__(self):
+#         return f"{self.user.username}'s profile"
+
+#     def save(self, *args, **kwargs):
+#         super().save(*args, **kwargs)
+
+#         # Create profile picture thumbnail of size, then save
+#         size = (300, 300)
+#         with Image.open(self.image.path) as img:
+#             if img.height > 300 or img.width > 300:
+#                 img.thumbnail(size)
+#                 img.save(self.image.path)
