9a916e1332f35202322504678f6c0b9d8126a0fc
zachmichael14 <zachmichael14@gmail.com>
2022-03-11 19:54:46 -0600

    Add User.pk to URLConfs for profile and profile edit view

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('profile/', user_views.profile, name='profile'),
+    path('<int:pk>/profile/', user_views.profile, name='profile'),
+    path('<int:pk>/profile/edit/', user_views.UserUpdateView.as_view(), name="edit"),
 ]
