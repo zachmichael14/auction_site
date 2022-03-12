bfe82883fef5390d8f60d94e0e67cf986d43742b
zachmichael14 <zachmichael14@gmail.com>
2022-03-11 19:56:09 -0600

    Reorder fields list for UserCreateForm
    The crispy template tag renders the username as the active field
    by default. Move username to first position so the first field is
    the active field.

--- users/forms.py
+++ users/forms.py
@@ -3,14 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class UserRegistrationForm(UserCreationForm):
+class UserCreateForm(UserCreationForm):
     first_name = forms.CharField(max_length=150)
     last_name = forms.CharField(max_length=150)
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
