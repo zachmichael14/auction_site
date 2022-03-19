2cb1896407c83d651b83afb1bfe1c24235a7202f
zachmichael14 <zachmichael14@gmail.com>
2022-03-19 18:32:42 -0500

    Remove unused import

--- auctions/templatetags/url_extras.py
+++ auctions/templatetags/url_extras.py
@@ -1,6 +1,4 @@
 from django import template
-from django.utils.http import urlencode
-
 
 register = template.Library()
 
@@ -8,7 +6,7 @@ register = template.Library()
 def query_params(context, **kwargs):
     # Get current URL query parameters
     d = context['request'].GET.copy()
-    # Add key:value pairs from tag function call
+    # Add key:value pairs from tag function call (template arguments)
     for k, v in kwargs.items():
         d[k] = v
     # Return encoded dictionary containing updated query params
