3af192c08b12960cbeedfa54c941f5fbd186bb85
zachmichael14 <zachmichael14@gmail.com>
2022-03-19 18:38:21 -0500

    Create model methods for profile view
    Add methods that return sets of Listings for a given user instance.

--- users/models.py
+++ users/models.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import prefetch_related_objects
 from PIL import Image
 
-from users.managers import UserManager
+
 
 class AuctionUser(AbstractUser):
     image = models.ImageField(default='default_profile_pic.jpeg', upload_to='profile_pics')
-    objects = UserManager
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -17,3 +17,23 @@ class AuctionUser(AbstractUser):
             if img.height > 300 or img.width > 300:
                 img.thumbnail(size)
                 img.save(self.image.path)
+
+
+    def active(self):
+        return self.user_listings.filter(is_active=True)
+
+    def closed(self):
+        return self.user_listings.filter(is_active=False)
+
+    def won_listings(self):
+        return self.won.all()
+
+    def bids(self):
+        # Return list rather than QuerySet
+        queryset = [obj.listing for obj in self.placed_bids.prefetch_related('listing')]
+        # queryset = [obj.listing for obj in self.placed_bids.prefetch_related('listing')]
+        return queryset
+
+    def watched(self):
+        queryset = [obj.listing for obj in self.user_watchlist.prefetch_related('listing')]
+        return queryset
