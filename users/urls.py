df2a2837f9b12bd24d2ab2f7a0a8bf0175bb15ec
zachmichael14 <zachmichael14@gmail.com>
2022-03-19 19:09:30 -0500

    Add user.pk to edit view
    UserUpdateView requires user.pk. Also use user.pk to add test func
    to ensure user may only edit their own profile.

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('<int:pk>/profile/', user_views.ProfileView.as_view(), name='profile'),
-    path('<int:pk>/profile/edit/', user_views.UserUpdateView.as_view(), name="edit"),
-]
+    path('profile/', user_views.ProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/edit/', user_views.UserUpdateView.as_view(), name="edit"),
+    path('profile/q=<q_set>/', user_views.ProfileView.as_view(), name='profile'),
+    ]
