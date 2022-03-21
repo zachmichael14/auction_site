2a2071a51672bdda7f6cb5da313a9970320f82c2
zachmichael14 <zachmichael14@gmail.com>
2022-03-21 15:39:45 -0500

    Add UserDeleteView

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('profile/', user_views.ProfileView.as_view(), name='profile'),
-    path('profile/<int:pk>/edit/', user_views.UserUpdateView.as_view(), name="edit"),
-    path('profile/q=<q_set>/', user_views.ProfileView.as_view(), name='profile'),
+    path('<int:pk>/profile/', user_views.UserView.as_view(), name='profile'),
+    path('<int:pk>/profile/edit/', user_views.UserUpdateView.as_view(), name="edit"),
+    path('<int:pk>/profile/q=<q_set>/', user_views.UserView.as_view(), name='profile_search'),
+    # path('<int:pk>/profile/delete/', user_views.UserDeleteView.as_view(), name='delete'),
     ]
