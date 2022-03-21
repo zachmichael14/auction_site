a38d1c202e682e4b8284d78a29af66efdf1985d5
zachmichael14 <zachmichael14@gmail.com>
2022-03-21 15:40:14 -0500

    Move validation to form clean method

--- auctions/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def validate_bid(amount):
-    if amount < 0.01:
-        raise ValidationError('Your bid must be greater than or equal to $0.01.')
-    if amount > 500.00:
-        raise ValidationError('Your bid must be less than or equal to $500.00')
-    else:
-        return amount
