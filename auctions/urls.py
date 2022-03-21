8cf95a03921c82b2b7cb73cf0115b960bdb9524b
zachmichael14 <zachmichael14@gmail.com>
2022-03-21 15:32:45 -0500

    Add close and delete urls

--- auctions/urls.py
+++ auctions/urls.py
@@ -11,8 +11,12 @@ urlpatterns = [
     path('browse/', auction_views.BrowseListingView.as_view(), name='browse'),
     path('browse/?q_cat=<q_cat>/', auction_views.BrowseListingView.as_view(), name='browse'),
     path('listing/create/', auction_views.ListingCreateView.as_view(), name='create'),
-    path('listing/<int:listing_id>/add/', auction_views.add_to_watchlist, name='add_to_watchlist'),
-    path('listing/<int:listing_id>/remove/', auction_views.remove_from_watchlist, name='remove_from_watchlist'),
+    path('listing/<int:listing_id>/wlist_add/', auction_views.add_to_watchlist, name='add_to_watchlist'),
+    path('listing/<int:listing_id>/wlist_remove/', auction_views.remove_from_watchlist, name='remove_from_watchlist'),
     path('listing/<int:listing_id>/bid/', auction_views.create_bid, name='create_bid'),
     path('listing/<int:listing_id>/update/', auction_views.ListingUpdateView.as_view(), name='update'),
+    path('listing/<int:listing_id>/close/', auction_views.close_listing, name='close'),
+    path('listing/<int:listing_id>/delete/', auction_views.ListingDeleteView.as_view(), name='delete'),
+
+
 ]
