98af89b9dbef03887463cc24f5fcd9d085665af2
zachmichael14 <zachmichael14@gmail.com>
2022-03-21 15:20:31 -0500

    Add get_absolute_url method

--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models import prefetch_related_objects
 from PIL import Image
-
 
 
 class AuctionUser(AbstractUser):
     image = models.ImageField(default='default_profile_pic.jpeg', upload_to='profile_pics')
+
+    def __str__(self):
+        return self.username
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -18,22 +19,28 @@ class AuctionUser(AbstractUser):
                 img.thumbnail(size)
                 img.save(self.image.path)
 
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('users:profile', kwargs={'pk': self.pk})
 
     def active(self):
+        """Return user's active listings."""
         return self.user_listings.filter(is_active=True)
 
     def closed(self):
+        """Return user's inactive listings."""
         return self.user_listings.filter(is_active=False)
 
     def won_listings(self):
+        """Return listings in which user was top bidder when listing closed."""
         return self.won.all()
 
     def bids(self):
-        # Return list rather than QuerySet
+        """Return list of listings on which user has placed a bid."""
         queryset = [obj.listing for obj in self.placed_bids.prefetch_related('listing')]
-        # queryset = [obj.listing for obj in self.placed_bids.prefetch_related('listing')]
         return queryset
 
     def watched(self):
+        """Return list of listings in user's watchlist."""
         queryset = [obj.listing for obj in self.user_watchlist.prefetch_related('listing')]
         return queryset
