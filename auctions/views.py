ab9dd377cc935e461939149619ba845881973017
zachmichael14 <zachmichael14@gmail.com>
2022-03-21 15:33:35 -0500

    Add delete and close views

--- auctions/views.py
+++ auctions/views.py
@@ -4,10 +4,12 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import CreateView
+from django.views.generic import DeleteView
 from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic import UpdateView
@@ -20,21 +22,6 @@ from auctions.forms import SearchForm
 from auctions.models import Category 
 from auctions.models import Listing 
 from auctions.models import Watchlist
-
-
-def index(request):
-    new_arrivals = Listing.objects.new_arrivals()
-
-    # Exclude user's listings from appearing in new arrivals
-    if request.user.is_authenticated:
-        new_arrivals = Listing.objects.new_arrivals(user=request.user)
-
-    context = {
-        'popular': Category.objects.popular(),
-        'new_arrivals': new_arrivals,
-        'search_form': SearchForm()
-    }
-    return render(request, 'auctions/index.html', context)
 
 
 class ListingCreateView(LoginRequiredMixin, CreateView):
@@ -64,17 +51,34 @@ class ListingDetailView(DetailView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
+
         listing = self.get_object()
+        user = self.request.user
+        is_watched = False
 
         # Determine if listing is in user's watchlist
-        is_watched = False
-        if self.request.user.is_authenticated:
-            is_watched = listing.is_watched(self.request.user)
+        if user.is_authenticated:
+            is_watched = listing.is_watched(user)
         
         context['is_watched'] = is_watched
-        context['bid_form'] = BidForm()
+        context['bid_form'] = BidForm(listing)
         return context
 
+
+class ListingDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Listing
+    pk_url_kwarg = 'listing_id'
+    success_url = '/'
+
+    def get_success_url(self):
+        user = self.request.user
+        return user.get_absolute_url()
+
+    def test_func(self):
+        # Ensure only seller can delete listing
+        listing = self.get_object()
+        return listing.seller == self.request.user
+    
 
 class ListingUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Listing
@@ -82,17 +86,15 @@ class ListingUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     pk_url_kwarg = 'listing_id'
 
     def get_form_class(self):
+        listing = self.get_object()
         self.form_class = ListingUpdateForm
 
         # Allow editing of starting_bid if bidding hasn't begun
-        listing = self.get_object()
         if listing.top_bid == listing.starting_bid:
             self.form_class = ListingCreateForm
         return self.form_class
 
     def form_valid(self, form):
-        """If the form is valid, save the associated model."""
-
         # Calculate end date from duration
         duration = datetime.timedelta(days=form.instance.duration) 
         end_date = datetime.datetime.now() + duration
@@ -118,50 +120,68 @@ class BrowseListingView(ListView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
+        user = self.request.user
+        total = Listing.objects.active().count()
+        categories = Category.objects.with_counts()
+             
+        # Exclude user's listings from total and categories
+        if user.is_authenticated:
+            total = Listing.objects.active_exclude_user(user).count()
+            categories = Category.objects.with_counts_user(user)
         
+        # Get query parameter from either view kwargs (URLconf captured group) 
+        # or GET request (search form)
         if self.kwargs:
-            # Get q_cat parameter from view kwargs (URLconf captured group)
             q_cat = self.kwargs.get('q_cat', None)
         else:
-            # Get q_cat parameter from GET request (search form)
-            q_cat = self.request.GET.get('q_cat', None)
-
-        user = None
-        if self.request.user.is_authenticated:
-            user = self.request.user
+            q_cat = self.request.GET.get('q_cat', None)   
         
-        context['categories'] = Category.objects.with_counts(user)
+        context['categories'] = categories
         context['search_form'] = SearchForm()
         context['q_string'] = self.request.GET.get('q_string', None)
         context['q_cat'] = q_cat
-        context['total'] = Listing.objects.active(user).count()
+        context['total'] = total
         return context
 
     def get_queryset(self):
+        queryset = self.model._default_manager.active()
+        user = self.request.user
+
+        # Exclude user's listings
+        if user.is_authenticated:
+            queryset = self.model._default_manager.active_exclude_user(user)
+
+        # Get query parameters from view kwargs (URLconf captured group; only q_cat captured)
+        # or GET request (search form; one or both params)
         if self.kwargs:
-            # Get q_cat parameter from view kwargs (URLconf captured group)
             q_cat = self.kwargs.get('q_cat', None)
             q_string = None
         else:
-            # Get query parameters from GET request (search form)
             q_string = self.request.GET.get('q_string', None)
             q_cat = self.request.GET.get('q_cat', None)
-        
-        queryset = super().get_queryset().exclude(is_active=False)
-
-        # Exclude user's listings
-        if self.request.user.is_authenticated:
-            queryset = queryset.exclude(seller=self.request.user)
  
-        # Match q_string to listing titles
+        # Match query param(s) to listing titles
         if q_string:
             queryset = queryset.filter(title__icontains=q_string)
-
-        # Match q_cat string to unique category name
         if q_cat:
             queryset = queryset.filter(category__name__contains=q_cat)
         return queryset
 
+
+def index(request):
+    user = request.user
+    new_arrivals = Listing.objects.recent()
+
+    # Exclude user's listings from appearing in new arrivals
+    if user.is_authenticated:
+        new_arrivals = Listing.objects.recent_exclude_user(user)
+
+    context = {
+        'popular': Category.objects.popular(),
+        'new_arrivals': new_arrivals,
+        'search_form': SearchForm()
+    }
+    return render(request, 'auctions/index.html', context)
 
 @login_required
 def add_to_watchlist(request, listing_id):
@@ -185,20 +205,34 @@ def remove_from_watchlist(request, listing_id):
 
 @login_required
 def create_bid(request, listing_id):
+    listing = Listing.objects.get(pk=listing_id)
     if request.method == 'POST':
-        bid_form = BidForm(request.POST)
-
+        bid_form = BidForm(listing=listing, data=request.POST)
         if bid_form.is_valid():
-            return render(request, 'auctions/data.html', {"context": bid_form.changed_data})
-
             bid = bid_form.save(commit=False)
 
-            bid.listing = Listing.objects.get(pk=listing_id)
+            # Set fields that were excluded from form
+            bid.listing = listing
             bid.bidder = request.user
             bid.save()
 
             messages.success(request, f"Bid successful: ${bid.amount} on {bid.listing.title}.")
             return HttpResponseRedirect(reverse('auctions:listing', args=(listing_id,)))
+
+    context = {
+        'listing': listing,
+        'bid_form': bid_form
+    }
+    return render(request, 'auctions/listing.html', context)
+
+@login_required
+def close_listing(request, listing_id):
+    if request.method == 'POST':
+        listing = Listing.objects.get(pk=listing_id)
+        if listing.seller == request.user:
+            listing.winner = listing.get_winner()
+            listing.is_active = False
+            listing.save()
+            return HttpResponseRedirect(reverse('auctions:listing', args=(listing_id,)))
         else:
-            messages.error(request, f"Error: {bid_form.errors}")
-            return HttpResponseRedirect(reverse('auctions:listing', args=(listing_id,))) 
+            raise PermissionDenied
