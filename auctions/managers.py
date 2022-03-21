2f3bb2e36a52ff68094e213815ad0ff3101c9067
zachmichael14 <zachmichael14@gmail.com>
2022-03-21 15:20:51 -0500

    Create new methods to exclude user listings
    Previous methods allowed an optional user argument that would
    exclude user's listings from returned QuerySet. This functionality
    was separated into new methods that exclude user's listings by
    default.

--- auctions/managers.py
+++ auctions/managers.py
@@ -9,22 +9,35 @@ class CategoryManager(Manager):
         active_count = Count('listings', filter=Q(listings__is_active=True))
         return self.get_queryset().annotate(actives=active_count).order_by('-actives')[:n]
 
-    def with_counts(self, user=None):
+    def with_counts(self):
         # Return QuerySet of categories and a count of active listings in that category
         count = Count('listings', filter=Q(listings__is_active=True))
-        # If user, exclude user's listings from both QuerySet and count
-        if user:
-            count = Count('listings', filter=Q(listings__is_active=True) & ~Q(listings__seller=user))
         return self.get_queryset().annotate(count=count)
 
+    def with_counts_user(self, user):
+        # Exclude user's listings from count and QuerySet 
+        count = Count('listings', filter=Q(listings__is_active=True) & ~Q(listings__seller=user))
+        return self.get_queryset().annotate(count=count)
+
+
 class ListingManager(Manager):
-    def active(self, user=None):
-        # Return active listings. If user, exclude user's listings from results
-        if user:
-            return self.exclude(is_active=False).exclude(seller=user)
+    def active(self):
+        # Return all active listings
         return self.exclude(is_active=False)
 
-    def new_arrivals(self, user=None, n=6):
+    def active_exclude_user(self, user):
+        # Return all active listings excluding user's
+        return self.active().exclude(seller=user)
+
+    def recent(self, n=6):
         # Return n most recently created listings
-        return self.active(user).order_by('-creation_timestamp')[:n]
+        return self.active().order_by('-creation_timestamp')[:n]
+    
+    def recent_exclude_user(self, user, n=6):
+        # Return n most recently created listings, excluding user's
+        return self.active_exclude_user(user).order_by('-creation_timestamp')[:n]
+
+
+
+
 
