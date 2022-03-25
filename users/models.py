e8189d666f67e1c54fd57ce54d33b5fc02756585
zachmichael14 <zachmichael14@gmail.com>
2022-03-25 13:24:32 -0500

    Remove save method
    Since AWS S3 buckets are now used for file storage, the save
    method no longer works for resizing images. In the future, an AWS
    lambda function may be used to resize images.

--- users/models.py
+++ users/models.py
@@ -9,16 +9,6 @@ class AuctionUser(AbstractUser):
     def __str__(self):
         return self.username
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        # Create profile picture thumbnail of size, then save
-        size = (300, 300)
-        with Image.open(self.image.path) as img:
-            if img.height > 300 or img.width > 300:
-                img.thumbnail(size)
-                img.save(self.image.path)
-
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('users:profile', kwargs={'pk': self.pk})
