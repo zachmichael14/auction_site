9ff4bde90e4c8e78dbe272d76bab121b64dbda50
zachmichael14 <zachmichael14@gmail.com>
2023-10-20 12:57:14 -0500

    Allow back4app host

--- ibuy/settings.py
+++ ibuy/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = [
-    'django-auction-ibuy.herokuapp.com',
+    '*',
     '127.0.0.1'
     ]
 
